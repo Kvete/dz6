c6f0bf83f5b6584133acacab6bab68150a565505
Anastasya <kvete@icloud.com>
2022-10-06 15:01:12 +0300

    решил 2 задачи

---
+++ задача41/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+Console.WriteLine("введите числа через пробел");
+int[] mas = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int amountOfPositiveNumbers(int [] array)
+{
+    int count=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) count++; 
+    }
+    return count;
+}
+Console.WriteLine($"количество положительных чисел={amountOfPositiveNumbers(mas)}");
---
+++ задача43/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+ значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/ //                                                  0  1  2  3
+Console.WriteLine("введите коэффициэнты через пробел: b1 k1 b2 k2");
+double[] mas = Console.ReadLine().Split().Select(double.Parse).ToArray();
+double x=0, y=0;
+if (mas[1]==mas[3]) Console.WriteLine("прямые параллельны т.к k1=k2");
+else 
+{
+    x=(mas[0]-mas[2])/(mas[3]-mas[1]);
+    y=mas[1]*x+mas[0];
+    Console.WriteLine($"координаты точки пересечения ({x}, {y})");
+}
